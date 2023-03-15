dd8fb631b3d32e51860bdd09ff62147fbeaaacf3
huaweicloud <hwcloudsolution@163.com>
2023-03-15 14:38:27 +0800

    补充遗漏环境变量
    Match-id-a65254e4f5b732e6d0b4c71d7de393ecf673738e

--- doc/build/fleetmanager/fleetmanager_run.sh
+++ doc/build/fleetmanager/fleetmanager_run.sh
@@ -50,6 +50,7 @@ export DEFAULT_SCRIPT_PATH=metaspace/image_env.sh # 镜像环境配置脚本 OBS
 export DEFAULT_AUXPROXY_PATH=metaspace/auxproxy.zip # AuxProxy组件压缩包 OBS路径 用于镜像环境配置 桶名/对象名
 export IMAGE_DISK_SIZE=40 # 用于镜像打包的ECS默认磁盘大小
 export DEFAULT_IMAGE_FLAVOR=s6.large.2 # 用于镜像打包的ECS默认规格
+export PROFILE_STORAGE_REGION={region} # 管理账号存放auxproxy及部署脚本的区域
 
 # 服务其他配置
 export AASS_ENABLE_HMAC=false							# aass是否开启hmac验证
